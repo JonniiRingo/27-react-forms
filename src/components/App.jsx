b1ccee9189c3738e738f37de6b2ad1a9c81b0c10
Jonnii Ringo <jonathansamuels@Jonathans-MBP-2.lan>
2024-12-26 23:53:53 -0800

    Solved the first challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const[name, setName] = useState("");
+  const[content, setContent] = useState("");
+
+  function handleChange(event){
+    setName(event.target.value);
+  }
+
+  function handleContent(event){
+    setContent(name);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {content}</h1>
+      <input 
+          onChange = {handleChange}
+          type="text" 
+          placeholder="What's your name?" 
+          value = {name}
+      />
+          
+      <button onClick = {handleContent}>Submit</button>
     </div>
   );
 }
